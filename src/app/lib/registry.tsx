66bd3ca15e123a927699f2437391e46b0487aed8
tuiza <luiza8.marlene@gmail.com>
2023-11-16 22:15:09 -0300

    feat: configura o styled compontes

---
+++ src/app/lib/registry.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import React, { useState } from 'react'
+import { useServerInsertedHTML } from 'next/navigation'
+import { ServerStyleSheet, StyleSheetManager, ThemeProvider, useTheme } from 'styled-components'
+import themes from '../../theme/index'
+
+export default function StyledComponentsRegistry({
+    children,
+}: Readonly<{
+    children: React.ReactNode
+}>) {
+    const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet())
+
+    useServerInsertedHTML(() => {
+        const styles = styledComponentsStyleSheet.getStyleElement()
+        styledComponentsStyleSheet.instance.clearTag()
+        return styles
+    })
+
+    if (typeof window !== 'undefined') return <>{children}</>
+
+    return (
+        <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
+            {children}
+        </StyleSheetManager>
+    )
+}
